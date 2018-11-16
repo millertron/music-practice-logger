700dab37447906d8a1c2585a7e939b1b86cdf093
Koha Choji <koha.choji@vistair.com>
2018-11-16 16:05:52 +0000

    Android project with empty activity

---
+++ app/src/main/java/com/millertronsmythe/musicpracticelogger/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.millertronsmythe.musicpracticelogger
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
